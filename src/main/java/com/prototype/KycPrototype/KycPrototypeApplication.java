4e8b7a90fdb7e46ed5546c5ef4efd24c3370a30b
shashwat kumar shrivastava <shashwatkumar05@gmail.com>
2025-07-12 13:57:04 +0530

    Initial Commit

---
+++ src/main/java/com/prototype/KycPrototype/KycPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.KycPrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prototype/KycPrototype/KycPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.KycPrototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
